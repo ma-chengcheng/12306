7bc64a425ade090b9e5a8f8a72147f8c2ee17b97
mamachengcheng <616254086@qq.com>
2021-03-04 10:42:49 +0800

    update srv/ticket

--- srv/ticket/go.mod
+++ srv/ticket/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/golang/protobuf v1.4.3
+	github.com/mamachengcheng/12306/common v0.0.0-20210303162504-b0e1a3fa8eda // indirect
 	github.com/micro/go-micro/v2 v2.9.1
 	github.com/micro/go-plugins/registry/consul/v2 v2.9.1
 	github.com/micro/micro/v2 v2.9.3 // indirect
--- srv/ticket/main.go
+++ srv/ticket/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	common "github.com/mamachengcheng/12306/common"
+	"github.com/mamachengcheng/12306/common"
 	"github.com/mamachengcheng/12306/srv/ticket/domain/respository"
 	s "github.com/mamachengcheng/12306/srv/ticket/domain/service"
 	"github.com/mamachengcheng/12306/srv/ticket/handler"
@@ -9,15 +9,18 @@ import (
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-plugins/registry/consul/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"log"
+	"strconv"
 )
 
 func main() {
-	//consulConfig,err := common.GetConsulConfig("127.0.0.1",8500,"/micro/config")
+	consulConfig, err := common.GetConsulConfig("127.0.0.1", 8500, "/micro/config")
 
-	//if err != nil {
-	//	log.Fatalf("Open MySQL database: %v", err)
-	//}
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
 
 	consulRegistry := consul.NewRegistry(func(options *registry.Options) {
 		options.Addrs = []string{
@@ -33,11 +36,21 @@ func main() {
 	)
 	srv.Init()
 
-	userDataService := s.NewTicketDataService(respository.NewTicketRepository(db))
+	mysqlInfo := common.GetMysqlFromConsul(consulConfig, "mysql")
+	dsn := mysqlInfo.User + ":" + mysqlInfo.Password + "@tcp(" + mysqlInfo.Host + strconv.FormatInt(mysqlInfo.Port, 10) + ")/" + mysqlInfo.DB + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		// 缓存预编译语句
+		PrepareStmt: true,
+	})
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	ticketDataService := s.NewTicketDataService(respository.NewTicketRepository(db))
 
 	// Register handler
 
-	ticket.RegisterTicketHandler(srv.Server(), &handler.Ticket{TicketDataService: userDataService})
+	ticket.RegisterTicketHandler(srv.Server(), &handler.Ticket{TicketDataService: ticketDataService})
 
 	// Run service
 	if err := srv.Run(); err != nil {
