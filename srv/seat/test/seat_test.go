5c3925651ae0d4c35397e0c2e418d927d1154e7d
mamachengcheng <616254086@qq.com>
2021-03-07 19:30:29 +0800

    update srv/seat/test/seat_test.go

--- srv/seat/test/seat_test.go
+++ srv/seat/test/seat_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-func TestHelloWorld(t *testing.T) {
+func TestCountRemainingSeats(t *testing.T) {
 	consulRegistry := consul.NewRegistry(func(options *registry.Options) {
 		options.Addrs = []string{
 			"127.0.0.1:8500",
@@ -34,5 +34,60 @@ func TestHelloWorld(t *testing.T) {
 		log.Printf("%v", err)
 	} else {
 		log.Printf("%v", cuntRemainingSeatsReply.Number)
+	}
+}
+
+func TestGetSeats(t *testing.T) {
+	consulRegistry := consul.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{
+			"127.0.0.1:8500",
+		}
+	})
+
+	srv := micro.NewService(
+		micro.Name("go.micro.service.payment.client"),
+		micro.Version("latest"),
+		micro.Registry(consulRegistry),
+	)
+
+	ticketService := goMicroServiceSeat.NewSeatService("go.micro.service.seat", srv.Client())
+
+	getSeatsSeatsReply, err := ticketService.GetSeats(context.TODO(), &goMicroServiceSeat.GetSeatsRequest{
+		SeatType:       0,
+		ScheduleStatus: 0,
+		Number:         0,
+		TrainID:        0,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+	} else {
+		log.Printf("%v", getSeatsSeatsReply.IsSuccess)
+		log.Printf("%v", getSeatsSeatsReply.SeatIDs)
+	}
+}
+
+func TestRollbackSeat(t *testing.T) {
+	consulRegistry := consul.NewRegistry(func(options *registry.Options) {
+		options.Addrs = []string{
+			"127.0.0.1:8500",
+		}
+	})
+
+	srv := micro.NewService(
+		micro.Name("go.micro.service.payment.client"),
+		micro.Version("latest"),
+		micro.Registry(consulRegistry),
+	)
+
+	ticketService := goMicroServiceSeat.NewSeatService("go.micro.service.seat", srv.Client())
+
+	rollbackSeatReply, err := ticketService.RollbackSeat(context.TODO(), &goMicroServiceSeat.RollbackSeatRequest{
+		SeatID:         0,
+		ScheduleStatus: 0,
+	})
+	if err != nil {
+		log.Printf("%v", err)
+	} else {
+		log.Printf("%v", rollbackSeatReply.IsSuccess)
 	}
 }
